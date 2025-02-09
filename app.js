51c4ccc1a33808403c1ada8af5d95eb228ba3a28
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-02-09 15:05:44 +0530

    mongodb setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 const app = express();
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
@@ -11,7 +12,8 @@ app.get("/", (req, res) => {
 	res.send("Welcome to subscription tracker API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 	console.log(`subscription tracker API is running on localhost:${PORT}`);
+	await connectToDatabase();
 });
 export default app;
--- config/env.js
+++ config/env.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 // Call config() with options
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV } = process.env;
+export const { PORT, NODE_ENV, DB_URI } = process.env;
